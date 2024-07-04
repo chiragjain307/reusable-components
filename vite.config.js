17b82949dd8130d76b1e28b6674797a4db056dc4
chiragjain307 <chiragjain.jain7@gmail.com>
2024-07-04 18:38:33 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,7 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/index.js'),
-      name: 'MyReactLibrary',
-      fileName: (format) => `my-react-library.${format}.js`
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM'
-        }
-      }
-    }
-  }
-});
+})
